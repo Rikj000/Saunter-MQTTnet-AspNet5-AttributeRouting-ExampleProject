9678a1447ba7531a1fee88a2ef8273f7b02631a7
Rikj000 <rik_helsen@hotmail.com>
2021-08-11 11:07:31 +0200

    :memo: Added documentation for MqttRouteHelper class (Unusable sadly)

--- Saunter-MQTTnet-AspNet5-AttributeRouting-ExampleProject/Helpers/MqttRouteHelper.cs
+++ Saunter-MQTTnet-AspNet5-AttributeRouting-ExampleProject/Helpers/MqttRouteHelper.cs
@@ -4,10 +4,16 @@ namespace Saunter_MQTTnet_AspNet5_AttributeRouting_ExampleProject.Helpers
 {
     public static class MqttRouteHelper
     {
+        /// <summary>
+        ///     Useless because C# Attribute properties need to be constants available at compile time.
+        ///     However still a good memory aid to quickly check the parsing difference between routes in:
+        ///     - Saunter
+        ///     - MQTTnet.AspNetCore.AttributeRouting
+        /// </summary>
         public static string ParseSaunterMqttRoute(string mqttNetRoute)
         {
             var saunterRoute = mqttNetRoute;
-            
+
             var singleEventWildcard = new Regex(@"{\w+:\w+}");
             if (singleEventWildcard.IsMatch(saunterRoute))
                 saunterRoute = singleEventWildcard.Replace(saunterRoute, "+");
@@ -15,7 +21,7 @@ namespace Saunter_MQTTnet_AspNet5_AttributeRouting_ExampleProject.Helpers
             var multiLevelWildcard = new Regex(@"{\*\w+}");
             if (multiLevelWildcard.IsMatch(saunterRoute))
                 saunterRoute = multiLevelWildcard.Replace(saunterRoute, "#");
-            
+
             return saunterRoute;
         }
     }
